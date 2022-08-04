453874d365ec5cab1d188896e836c633c919fa78
Dipesh Gupta <dipesh.gupta@w3villa.com>
2022-08-04 16:44:33 +0530

    Make app Production Ready

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -79,8 +79,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => 'emailfordipesh@gmail.com',
-    :password             => 'Diepsdf@134',
+    :user_name            => ENV['emailfordipesh@gmail.com'],
+    :password             => ENV['Diepsdf@134'],
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
