fd4b2a287cf70b793603775c3383295f07772f4c
Dipesh Gupta <dipesh.gupta@w3villa.com>
2022-08-04 17:17:38 +0530

    Deployed to Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,15 +99,15 @@ Rails.application.configure do
 
 
 config.action_mailer.delivery_method = :smtp
-host = 'localhost:3000' #replace with your own url
-config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
+host = 'warm-scrubland-94822.herokuapp.com' #replace with your own url
+config.action_mailer.default_url_options = { host: host }
 
 # SMTP settings for gmail
 config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :user_name            => 'emailfordipesh@gmail.com',
-  :password             => 'dieashph@132',
+  :password             => 'Dimpy#12',
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
